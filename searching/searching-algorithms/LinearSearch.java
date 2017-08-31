009019d9cfe5381f307bc26f2f603149cbe3e69e
Andrey Yemelyanov <andrey.yemelyanov.2@consultant.volvo.com>
2017-08-31 16:41:48 +0200

    Remove unnecesary imports

--- searching/searching-algorithms/LinearSearch.java
+++ searching/searching-algorithms/LinearSearch.java
@@ -1,11 +1,7 @@
 import java.util.*;
-import java.util.stream.*;
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
 import org.junit.Test;
-import org.junit.runner.JUnitCore;
-import org.junit.runner.Result;
-import org.junit.runner.notification.Failure;
 
 public class LinearSearch{
   public static int search(int[] arr, int key){
