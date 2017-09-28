e490766cba42562dd710e01c14ebb46fe959a00a
Andrey Yemelyanov <andrey.yemelyanov@gmail.com>
2017-09-28 10:00:48 +0200

    Increment arbitrary precision integer

--- Ch5Arrays.java
+++ Ch5Arrays.java
@@ -4,6 +4,33 @@ import static org.hamcrest.CoreMatchers.*;
 import org.junit.Test;
 
 public class Ch5Arrays{
+  static int[] increment(int[] x){
+    int[] y = new int[x.length + 1];
+    int carry = 0;
+    for(int i = x.length - 1; i >= 0; i--){
+      int add = 0;
+      if(i == x.length - 1) add = 1;
+      int sum = x[i] + add + carry;
+      if(sum >= 10) y[i + 1] = sum % 10; 
+      else y[i + 1] = sum;
+      carry = sum / 10;
+    }
+    if(carry != 0) y[0] = carry;
+    return y;
+  }
+  @Test
+  public void testIncrement(){
+    assertThat(increment(new int[]{1,2,9}), is(new int[]{0,1,3,0}));
+    assertThat(increment(new int[]{9,9,9}), is(new int[]{1,0,0,0}));
+    assertThat(increment(new int[]{1,2,3}), is(new int[]{0,1,2,4}));
+    assertThat(increment(new int[]{9}), is(new int[]{1,0}));
+    assertThat(increment(new int[]{1,9,2}), is(new int[]{0,1,9,3}));
+    assertThat(increment(new int[]{1,9,9}), is(new int[]{0,2,0,0}));
+    assertThat(increment(new int[]{0}), is(new int[]{0,1}));
+    assertThat(increment(new int[]{0,0,0,0}), is(new int[]{0,0,0,0,1}));
+    assertThat(increment(new int[]{0,0,0,9}), is(new int[]{0,0,0,1,0}));
+  }
+  
   static void partition(int[] a, int nGroups){
     int i = 0;
     for(int k = 0; k < nGroups; k++){
