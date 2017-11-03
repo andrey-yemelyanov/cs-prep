5ac72e15d80c3c3e067ffb94602f0d502f86dfe0
Andrey Yemelyanov <andrey.yemelyanov@gmail.com>
2017-11-03 08:39:13 +0100

    Eval RPN expression

--- Ch9StacksAndQueues.java
+++ Ch9StacksAndQueues.java
@@ -4,6 +4,54 @@ import static org.hamcrest.CoreMatchers.*;
 import org.junit.Test;
 
 public class Ch9StacksAndQueues{
+  static double evalRpn(String rpnExpr, char delimiter){
+    java.util.Stack<Double> s = new java.util.Stack<>();
+    int i = 0;
+    while(i < rpnExpr.length()){
+      String token = "";
+      while(i < rpnExpr.length() && rpnExpr.charAt(i) != delimiter) {
+        token += rpnExpr.charAt(i);
+        i++;
+      }
+      i++; // advance past the delimiter to the next valid char
+      switch(token){
+        case "+":
+          s.push(s.pop() + s.pop());
+          break;
+        case "-":
+          double b = s.pop();
+          double a = s.pop();
+          s.push(a - b);
+          break;
+        case "*":
+          s.push(s.pop() * s.pop());
+          break;
+        case "/":
+          b = s.pop();
+          a = s.pop();
+          s.push(a/b);
+          break;
+        default:
+          s.push(Double.parseDouble(token));
+      }
+    }
+    return s.pop();
+  }
+  @Test
+  public void testEvalRpn(){
+    assertThat(evalRpn("0",','), is(0.0));
+    assertThat(evalRpn("5",','), is(5.0));
+    assertThat(evalRpn("-5",','), is(-5.0));
+    assertThat(evalRpn("-5.112",','), is(-5.112));
+    assertThat(evalRpn("3,-4,*",','), is(-12.0));
+    assertThat(evalRpn("3,4,*",','), is(12.0));
+    assertThat(evalRpn("31,475,*",','), is(14725.0));
+    assertThat(evalRpn("-100,5,+,1,*",','), is(-95.0));
+    assertThat(evalRpn("1,1,+,-2,*",','), is(-4.0));
+    assertThat(evalRpn("3,-4,/,5,*",','), is(-3.75));
+    assertThat(evalRpn("3.6,-2,/,5,*",','), is(-9.0));
+  }
+  
   // Implements a stack with Max api. To ensure that pop, push and max have time complexity O(1)
   // for each entry in the stack, we store the max value at or below that entry. Space O(n).
   class Stack{
