1c358f76e19b994152c96678ae172e5da2d5d3d7
Andrey Yemelyanov <andrey.yemelyanov@gmail.com>
2017-12-08 18:40:45 +0100

    Find first element greater than K in sorted array

--- Ch12Searching.java
+++ Ch12Searching.java
@@ -4,6 +4,29 @@ import static org.hamcrest.CoreMatchers.*;
 import org.junit.Test;
 
 public class Ch12Searching{
+  int firstLargerThanK(int[] arr, int K){
+    int from = 0; int to = arr.length - 1;
+    while(from <= to){
+      int mid = from + (to - from) / 2;
+      if(arr[mid] == K) from = mid + 1;
+      else if(arr[mid] > K && ((mid - 1 >= 0 && arr[mid - 1] <= K) || mid == 0)) return mid;
+      else if(arr[mid] > K) to = mid - 1;
+      else from = mid + 1;
+    }
+    return -1;
+  }
+  @Test
+  public void testFirstLargerThanK(){
+    assertThat(firstLargerThanK(new int[]{-14,-10,2,108,108,243,245,245,245,401}, -13), is(1));
+    assertThat(firstLargerThanK(new int[]{-14,-10,2,108,108,243,245,245,245,401}, 1), is(2));
+    assertThat(firstLargerThanK(new int[]{-14,-10,2,108,108,243,245,245,245,401}, 100), is(3));
+    assertThat(firstLargerThanK(new int[]{-14,-10,2,108,108,243,245,245,245,401}, 108), is(5));
+    assertThat(firstLargerThanK(new int[]{-14,-10,2,108,108,243,245,245,245,401}, 245), is(9));
+    assertThat(firstLargerThanK(new int[]{-14,-10,2,108,108,243,245,245,245,401}, -15), is(0));
+    assertThat(firstLargerThanK(new int[]{-14,-10,2,108,108,243,245,245,245,401}, 500), is(-1));
+    assertThat(firstLargerThanK(new int[]{-14,-10,2,108,108,243,245,245,245,401}, 401), is(-1));
+  }
+
   int firstOccurrence(int[] arr, int key){
     int from = 0; int to = arr.length - 1;
     while(from <= to){
